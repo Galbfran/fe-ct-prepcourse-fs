f92b851372d353b96ea17d5469b48f9ed2ac154b
Galbfran <107511558+Galbfran@users.noreply.github.com>
2023-04-18 12:47:58 -0300

    subo ejercicios realizados

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,11 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   let arregloResultado = []
+   for (let clave in objeto){
+      arregloResultado.push([clave , objeto[clave]] )
+   }
+   return arregloResultado
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +19,27 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   let resultadoObjeto = {}
+   let stringArray = string.split('')
+   let stringArratOrdenado = stringArray.sort()
+   let clave = []
+   let valor = []
+
+   let contador = 1
+   for (let i = 0 ;i < stringArratOrdenado.length ; i++){
+      
+      if (stringArratOrdenado[i] == stringArratOrdenado[i+1] ){
+         contador += 1
+      }else {
+         clave.push(stringArratOrdenado[i])
+         valor.push(contador)
+         contador = 1
+      }
+   }
+   clave.forEach((elemento , indice) =>{
+      resultadoObjeto[elemento] = valor[indice]
+   })
+   return resultadoObjeto
 }
 
 function capToFront(string) {
@@ -22,6 +48,19 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   let stringArray = string.split('')
+   let mayusculas = []
+   let minusculas = []
+   let resultado = ""
+   stringArray.forEach((elemento) => {
+      if (elemento == elemento.toUpperCase()){
+         mayusculas.push(elemento)
+      } else {
+         minusculas.push(elemento)
+      }
+   })
+   resultado = mayusculas.join('') + minusculas.join('')
+   return resultado
 }
 
 function asAmirror(frase) {
@@ -29,18 +68,44 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   let stringArray = frase.split(" ")
+   let nuevoArray  = []
+   
+   stringArray.forEach((elemento) =>{
+      nuevoArray.push(elemento.split('').reverse().join(''))
+   })
+   let resultado = nuevoArray.join(" ")
+   return resultado
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   let numeroString = numero.toString()  //paso numero a string
+   // lo paso a array aplico reverse y lo vuelva pasar a texto
+   let numeroAlReves = numeroString.split('').reverse().join('')
+   if (numero == numeroAlReves){
+      return "Es capicua"
+   } else {
+      return "No es capicua"
+   }
+
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   let stringArray = string.split('');
+   for (let i = 0; i < stringArray.length; i++) {
+      if (stringArray[i] === 'a' || stringArray[i] === 'b' || stringArray[i] === 'c') {
+         stringArray.splice(i, 1);
+         i--; // para compensar el desplazamiento en el array
+      }
+   }
+   let resultado = stringArray.join('');
+   return resultado;
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +114,10 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   arrayOfStrings.sort(function(a, b) {
+      return a.length - b.length;
+   });
+   return arrayOfStrings;
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +127,15 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   resultado = []
+   array1.forEach((elemento) => {
+      array2.forEach((i) => {
+         if (elemento == i){
+            resultado.push(elemento)
+         }
+      })
+   })
+   return resultado
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
